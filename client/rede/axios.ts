a888afea8ab8fd03078137a4f6548b217a23d861
daniel <daniellealuau@gmail.com>
2023-11-26 13:16:29 -0300

    ok

--- client/rede/axios.ts
+++ client/rede/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const makeRequest = axios.create({
-    baseURL:'https://codpet-back-production.up.railway.app',
+    baseURL:'https://codpet-front-production.up.railway.app/api',
     withCredentials: true
 })
